3d4cbc464bdf2eb7ea916e4c70897e770365a585
Chelsea Walker <chelzwa@gmail.com>
2015-11-23 21:26:24 -0800

    Added functionality for adding new envelopes.

--- src/app/envelopes/view/envelopes.js
+++ src/app/envelopes/view/envelopes.js
@@ -5,28 +5,40 @@ function (EnvelopeSrvc) {
   var ctrl = this;
   
   var initData = function() {
-    var envelope = {
-      name: 'This is a test',
-      goal: 12,
-      user: 1
-    };
+    // var envelope = {
+    //   name: 'This is a test',
+    //   goal: 12,
+    //   user: 1
+    // };
 
-    EnvelopeSrvc.data.create(envelope)
+    // EnvelopeSrvc.data.create(envelope)
+    //   .then(function(envelopes) {
+    //     console.log('it worked');
+    //     console.log(envelopes);
+    //   })
+    //   .catch(function() {
+    //     console.log('something went wrong');
+    //   });
+
+    EnvelopeSrvc.data.findAll()
       .then(function(envelopes) {
-        console.log('it worked');
-        console.log(envelopes);
+        ctrl.envelopes = envelopes;
       })
       .catch(function() {
         console.log('something went wrong');
       });
+  };
 
-    EnvelopeSrvc.data.findAll()
-      .then(function(envelopes) {
-        console.log('it worked');
-        console.log(envelopes);
-      })
-      .catch(function() {
-        console.log('something went wrong');
+  ctrl.addEnvelope = function() {
+    ctrl.newEnvelope = EnvelopeSrvc.data.createInstance();
+    ctrl.addingEnvelope = true;
+  };
+
+  ctrl.saveEnvelope = function() {
+    EnvelopeSrvc.data.create(ctrl.newEnvelope)
+      .then(function(envelope) {
+        console.log(envelope);
+        ctrl.envelopes.push(envelope);
       });
   };
 
